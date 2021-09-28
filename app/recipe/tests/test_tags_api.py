857826be6ba4124563755ae269517733f040ea0e
Jeromy Coburn <jeromy.coburn@feedtrail.com>
2021-09-28 11:40:34 -0500

    Added create tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -25,6 +25,9 @@ class PrivateTagsApiTests(TestCase):
         'password': 'testpass',
         'name': 'Test Name'
     }
+    sampleTag = {
+        'name': 'Test tag'
+    }
 
     def setUp(self):
         self.user = get_user_model().objects.create_user(
@@ -57,3 +60,16 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        self.client.post(TAGS_URL, self.sampleTag)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=self.sampleTag['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        res = self.client.post(TAGS_URL, {'name': ''})
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
